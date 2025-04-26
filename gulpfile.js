8e7d61815e1fd9f52c561e5dda2bb8c629b50816
MateusPiresLino <mateuspireslino@gmail.com>
2025-04-26 01:44:59 -0300

    criando o hero e importando a font Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
-function style() {
+function styles() {
     return gulp.src('./src/styles/*.scss') //função para recuperar os arquivos. 
         .pipe(sass({style: 'compressed'})) //compressão dos arquivos que serão compilados. 
         .pipe(gulp.dest('./dist/css')); //compilação dos arquivos a pasta dist/destino.
 }
 
-exports.default = style;
+function images() {
+    return gulp.src('./src/images/**/*') //função para recuperar os arquivos. 
+        .pipe(imagemin()) //compressão dos arquivos que serão compilados. 
+        .pipe(gulp.dest('./dist/images')); //compilação dos arquivos a pasta dist/destino.
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(style))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
